e86ed4ec92bc6d2bd06e74af94768eecea65f18a
SusmeetaJ <sush.3996@gmail.com>
2021-05-01 14:50:11 +0530

    Added a component to select a year

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,30 +1,42 @@
+import React, { useState } from "react";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpenseFilter";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        date={props.item[0].date}
-        title={props.item[0].title}
-        amount={props.item[0].amount}
-      />
-      <ExpenseItem
-        date={props.item[1].date}
-        title={props.item[1].title}
-        amount={props.item[1].amount}
-      />
-      <ExpenseItem
-        date={props.item[2].date}
-        title={props.item[2].title}
-        amount={props.item[2].amount}
-      />
-      <ExpenseItem
-        date={props.item[3].date}
-        title={props.item[3].title}
-        amount={props.item[3].amount}
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpenseItem
+          date={props.item[0].date}
+          title={props.item[0].title}
+          amount={props.item[0].amount}
+        />
+        <ExpenseItem
+          date={props.item[1].date}
+          title={props.item[1].title}
+          amount={props.item[1].amount}
+        />
+        <ExpenseItem
+          date={props.item[2].date}
+          title={props.item[2].title}
+          amount={props.item[2].amount}
+        />
+        <ExpenseItem
+          date={props.item[3].date}
+          title={props.item[3].title}
+          amount={props.item[3].amount}
+        />
+      </Card>
+    </div>
   );
 };
 export default Expenses;
